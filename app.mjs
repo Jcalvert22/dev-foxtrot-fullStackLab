a71fb98214118e93570161e55df800827cbc33b0
Calvert <jcalvert6@una.edu>
2025-12-01 10:22:05 -0600

    commit file

--- app.mjs
+++ app.mjs
@@ -1,13 +1,23 @@
-import express from 'express'
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+
+
 
 const app = express()
+// const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
 app.get('/api/classData', (req, res) => {
-    res.sendFile(stuData.json);
+    res.sendFile(join(__dirname, 'stuData.json'))
   })
 
 app.listen(3000)
